90dc96080a45272c76463a888f866354b0c5c39e
LeeHungu4122 <ilopio@naver.com>
2019-10-05 17:30:02 +0900

    롬복이 제대로 작동하지 않을 때 조치(source 메뉴칸에서 지정.)

--- src/main/java/com/example/demo/controller/ChatController.java
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -14,30 +14,30 @@ import com.example.demo.dto.ChatMessageDto;
 @Controller
 public class ChatController {
 	private List<ChatMessageDto> messages;
-	
+
 	ChatController() {
 		messages = new ArrayList<>();
 	}
-	
+
 	@RequestMapping("/chat/main")
 	public String showMain() {
 		return "chat/main";
 	}
-	
+
 	@RequestMapping("/chat/addMessage")
 	@ResponseBody
 	public Map addMessage(String writer, String body) {
 		long id = messages.size() + 1;
 		messages.add(new ChatMessageDto(id, writer, body));
-		
+
 		Map<String, Object> rs = new HashMap<>();
-		 
+
 		rs.put("msg", "채팅 메세지가 추가되었습니다.");
 		rs.put("resultCode", "S-1");
-		
+
 		return rs;
 	}
-	
+
 	@RequestMapping("/chat/getAllMessages")
 	@ResponseBody
 	public List<ChatMessageDto> getAllMessages() {
--- src/main/java/com/example/demo/dto/ChatMessageDto.java
+++ src/main/java/com/example/demo/dto/ChatMessageDto.java
@@ -3,13 +3,48 @@ package com.example.demo.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 public class ChatMessageDto {
+
+	@Override
+	public String toString() {
+		return "ChatMessageDto [id=" + id + ", writer=" + writer + ", body=" + body + "]";
+	}
+
+	public ChatMessageDto(long id, String writer, String body) {
+		super();
+		this.id = id;
+		this.writer = writer;
+		this.body = body;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
 	private long id;
 	private String writer;
 	private String body;
